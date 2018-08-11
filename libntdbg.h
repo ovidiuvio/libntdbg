d9a53c7a8c7ec943ac55a3c1b038f9d2a2827082
Ovidiu Ionescu <ovidiu@vsdebug.pro>
2018-08-12 01:49:37 +0300

    [+] Added read / write checks for memory

--- libntdbg.h
+++ libntdbg.h
@@ -15,20 +15,20 @@
 -------------------------------------------------------------------------
 */
 
-#define NTDBG_RESULT						 INT32
+#define NTDBG_RESULT                         INT32
 
-#define NTDBG_OK								 0 
-#define NTDBG_FAIL							-1
-#define NTDBG_INVALID_ADDRESS					-2
-#define NTDBG_ACCESS_DENIED					-3
-#define NTDBG_INVALID_SIZE					-4
+#define NTDBG_OK                                 0 
+#define NTDBG_FAIL                          -1
+#define NTDBG_INVALID_ADDRESS                   -2
+#define NTDBG_ACCESS_DENIED                 -3
+#define NTDBG_INVALID_SIZE                  -4
 
 /*
 -------------------------------------------------------------------------
 // NTDBG defines
 -------------------------------------------------------------------------
 */
-#define NTDBG_PAGE_SIZE						4096
+#define NTDBG_PAGE_SIZE                     4096
 
 /*
 -------------------------------------------------------------------------
@@ -36,14 +36,13 @@
 -------------------------------------------------------------------------
 */
 
-#ifdef	NTDBG_EXPORTS
-#define NTDBG_API							__declspec(dllexport)
+#ifdef  NTDBG_EXPORTS
+#define NTDBG_API                           __declspec(dllexport)
 #else
-#define NTDBG_API							__declspec(dllimport)
-#endif	
+#define NTDBG_API                           __declspec(dllimport)
+#endif  
 
-#define NTDBG_CALL						__stdcall
-#define NTDBG_FASTCALL					__fastcall
+#define NTDBG_CALL                          __cdecl
 
 /*
 -------------------------------------------------------------------------
@@ -51,7 +50,7 @@
 -------------------------------------------------------------------------
 */
 
-#define NTDBG_INLINE						__forceinline
+#define NTDBG_INLINE                        __forceinline
 
 /*
 -------------------------------------------------------------------------
@@ -79,9 +78,9 @@ extern "C"
     NTDBG_API
     HANDLE
     NTDBG_CALL
-    NtDbgOpenProcess			(	IN	DWORD	dwDesiredAccess,
-                                    IN	BOOL	bInheritHandle,
-                                    IN	DWORD	dwProcessId	);
+    NtDbgOpenProcess            (   IN  DWORD   dwDesiredAccess,
+                                    IN  BOOL    bInheritHandle,
+                                    IN  DWORD   dwProcessId );
 
     /*
     -------------------------------------------------------------------------
@@ -91,7 +90,7 @@ extern "C"
     NTDBG_API
     NTDBG_RESULT
     NTDBG_CALL
-    NtDbgCloseHandle			(   IN  HANDLE	hObject		);
+    NtDbgCloseHandle            (   IN  HANDLE  hObject     );
 
 
     /*
@@ -102,11 +101,11 @@ extern "C"
     NTDBG_API
     NTDBG_RESULT
     NTDBG_CALL
-    NtDbgReadProcessMemory		(	IN	HANDLE	hProcess,
-                                    IN	DWORD64	lpBaseAddress,
-                                    OUT	LPVOID	lpBuffer,
-                                    OUT	SIZE_T	nSize,
-                                    OUT	SIZE_T*	lpNumberOfBytesRead );
+    NtDbgReadProcessMemory      (   IN  HANDLE  hProcess,
+                                    IN  DWORD64 lpBaseAddress,
+                                    OUT LPVOID  lpBuffer,
+                                    OUT SIZE_T  nSize,
+                                    OUT SIZE_T* lpNumberOfBytesRead );
 
     /*
     -------------------------------------------------------------------------
@@ -116,41 +115,41 @@ extern "C"
     NTDBG_API
     NTDBG_RESULT
     NTDBG_CALL
-    NtDbgWriteProcessMemory		(   IN  HANDLE	hProcess,
-                                    IN  DWORD64	lpBaseAddress,
+    NtDbgWriteProcessMemory     (   IN  HANDLE  hProcess,
+                                    IN  DWORD64 lpBaseAddress,
                                     IN  LPVOID  lpBuffer,
-                                    IN  SIZE_T	nSize,
-                                    OUT SIZE_T*	lpNumberOfBytesWritten );
+                                    IN  SIZE_T  nSize,
+                                    OUT SIZE_T* lpNumberOfBytesWritten );
 
     NTDBG_API
     NTDBG_RESULT
     NTDBG_CALL
-    NtDbgProcessMemCpy			(	IN	HANDLE	hSrcProcess,
-                                    IN	HANDLE	hDstProcess,
-                                    IN	DWORD64	lpSrcAddress,
-                                    IN	DWORD64	lpDstAddress,
-                                    IN	SIZE_T	nSize,
-                                    OUT	SIZE_T*	lpNumberOfBytesCopyed );
+    NtDbgProcessMemCpy          (   IN  HANDLE  hSrcProcess,
+                                    IN  HANDLE  hDstProcess,
+                                    IN  DWORD64 lpSrcAddress,
+                                    IN  DWORD64 lpDstAddress,
+                                    IN  SIZE_T  nSize,
+                                    OUT SIZE_T* lpNumberOfBytesCopyed );
 
     NTDBG_API
     NTDBG_RESULT
     NTDBG_CALL
-    NtDbgProcessMemSet			(	IN	HANDLE	hProcess,
-                                    IN	DWORD64	lpBaseAddress,
-                                    IN  DWORD	uByte,
-                                    IN	SIZE_T	nSize,									
-                                    OUT	SIZE_T*	lpNumberOfBytesSet );
+    NtDbgProcessMemSet          (   IN  HANDLE  hProcess,
+                                    IN  DWORD64 lpBaseAddress,
+                                    IN  DWORD   uByte,
+                                    IN  SIZE_T  nSize,
+                                    OUT SIZE_T* lpNumberOfBytesSet );
 
     NTDBG_API
     DWORD64
     NTDBG_CALL
-    NtDbgProcessAlloc			(	IN		HANDLE	 hProcess,
-                                    IN		SIZE_T	 nSize );
+    NtDbgProcessAlloc           (   IN      HANDLE   hProcess,
+                                    IN      SIZE_T   nSize );
 
     NTDBG_API
     NTDBG_RESULT
     NTDBG_CALL
-    NtDbgProcessFree			(	IN		HANDLE	hProcess,
-                                    IN		DWORD64	lpAddress );
+    NtDbgProcessFree            (   IN      HANDLE  hProcess,
+                                    IN      DWORD64 lpAddress );
 }
 #endif
